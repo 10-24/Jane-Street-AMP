8f712d81f5b30c309e4799369d9924e73122386d
Ryan <117957183+10-24@users.noreply.github.com>
2025-03-19 07:54:21 -0500

    Created a-write-an-mpl-program-whose-length-is0shorter-than-its-output-length.ts

---
+++ Minimalist Programming Language/a-write-an-mpl-program-whose-length-is0shorter-than-its-output-length.ts
@@ -0,0 +1,10 @@
+let a;
+a=9999999;a*a
+const asMpl = "a=9999999;a*a"
+const output = (a*a).toString()
+console.log(`Output ${output}`)
+console.log(`Output Characters: ${output.length}`)
+console.log(`mpl.length ${asMpl.length}`)
+// Output 99999980000001
+// Output Characters: 14
+// mpl.length 13
